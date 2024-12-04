b6814a20734305bbf7895df6f6f72f01953d1cbf
LundiLanga <alulundilanga@gmail.com>
2024-12-04 08:52:50 +0200

    Arranged the movie cards in horizontal order

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-
 class MovieCard extends Component {
   render() {
-    const { title, genre, rating, description } = this.props.movie;
-    
+    const { title, genre, rating, description, releaseDate, posterUrl } = this.props.movie;
+
     return (
       <div className="movie-card">
+        <img src={posterUrl} alt={`${title} poster`} className="movie-poster" />
         <h3>{title}</h3>
         <p>{genre}</p>
         <p>{rating} / 10</p>
         <p>{description}</p>
+        <p><strong>Release Date:</strong> {releaseDate}</p>
       </div>
     );
   }
@@ -23,6 +24,8 @@ MovieCard.propTypes = {
     genre: PropTypes.string.isRequired,
     rating: PropTypes.number.isRequired,
     description: PropTypes.string.isRequired,
+    releaseDate: PropTypes.string.isRequired,
+    posterUrl: PropTypes.string.isRequired,
   }).isRequired,
 };
 
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,26 +3,26 @@ import MovieCard from './MovieCard';
 
 const MovieList = () => {
   const movies = [
-    { title: "Inception", genre: "Sci-Fi", rating: 9, description: "A mind-bending thriller by Christopher Nolan." },
-    { title: "The Dark Knight", genre: "Action", rating: 9.5, description: "The epic Batman sequel." },
-    { title: "Interstellar", genre: "Sci-Fi", rating: 8.6, description: "A journey through space and time to save humanity." },
-    { title: "The Matrix", genre: "Sci-Fi", rating: 8.7, description: "A hacker discovers the shocking reality of his world." },
-    { title: "Parasite", genre: "Thriller", rating: 8.6, description: "A gripping tale of class struggles in South Korea." },
-    { title: "Avengers: Endgame", genre: "Action", rating: 8.4, description: "The Avengers unite for their final showdown." },
-    { title: "Forrest Gump", genre: "Drama", rating: 8.8, description: "The inspiring life story of a kind-hearted man." },
-    { title: "The Godfather", genre: "Crime", rating: 9.2, description: "The story of a powerful Italian-American crime family." },
-    { title: "Pulp Fiction", genre: "Crime", rating: 8.9, description: "An intertwined series of tales from the criminal underworld." },
-    { title: "The Shawshank Redemption", genre: "Drama", rating: 9.3, description: "A story of hope and friendship inside a prison." },
+    { title: "Inception", genre: "Sci-Fi", rating: 9, description: "A mind-bending thriller by Christopher Nolan.", posterUrl: "https://image-url/inception.jpg"},
+    { title: "The Dark Knight", genre: "Action", rating: 9.5, description: "The epic Batman sequel.", posterUrl:"https://image-url/the-dark-knight.jpg"},
+    { title: "Interstellar", genre: "Sci-Fi", rating: 8.6, description: "A journey through space and time to save humanity.", posterUrl: "https://image-url/inception.jpg" },
+    { title: "The Matrix", genre: "Sci-Fi", rating: 8.7, description: "A hacker discovers the shocking reality of his world.", posterUrl: "https://image-url/inception.jpg" },
+    { title: "Parasite", genre: "Thriller", rating: 8.6, description: "A gripping tale of class struggles in South Korea.", posterUrl: "https://image-url/inception.jpg"},
+    { title: "Avengers: Endgame", genre: "Action", rating: 8.4, description: "The Avengers unite for their final showdown.", posterUrl: "https://image-url/inception.jpg" },
+    { title: "Forrest Gump", genre: "Drama", rating: 8.8, description: "The inspiring life story of a kind-hearted man.", posterUrl: "https://image-url/inception.jpg" },
+    { title: "The Godfather", genre: "Crime", rating: 9.2, description: "The story of a powerful Italian-American crime family.", posterUrl: "https://image-url/inception.jpg" },
+    { title: "Pulp Fiction", genre: "Crime", rating: 8.9, description: "An intertwined series of tales from the criminal underworld.", posterUrl: "https://image-url/inception.jpg" },
+    { title: "The Shawshank Redemption", genre: "Drama", rating: 9.3, description: "A story of hope and friendship inside a prison.", posterUrl: "https://image-url/inception.jpg" },
     { title: "Titanic", genre: "Romance", rating: 7.8, description: "An epic love story set on the doomed Titanic." },
-    { title: "Gladiator", genre: "Action", rating: 8.5, description: "A Roman general seeks revenge for his family's murder." },
+    { title: "Gladiator", genre: "Action", rating: 8.5, description: "A Roman general seeks revenge for his family's murder.", posterUrl: "https://image-url/inception.jpg" },
     { title: "Joker", genre: "Drama", rating: 8.4, description: "The origin story of Gotham's infamous villain." },
-    { title: "The Lord of the Rings: The Return of the King", genre: "Fantasy", rating: 9, description: "The final battle for Middle-earth." },
-    { title: "Star Wars: Episode V - The Empire Strikes Back", genre: "Sci-Fi", rating: 8.7, description: "The Rebel Alliance faces the mighty Empire." },
-    { title: "Fight Club", genre: "Drama", rating: 8.8, description: "A man's ordinary life spirals into chaos." },
-    { title: "The Lion King", genre: "Animation", rating: 8.5, description: "A young lion learns to embrace his destiny." },
-    { title: "Black Panther", genre: "Action", rating: 7.3, description: "A new king must defend Wakanda from enemies." },
-    { title: "Spider-Man: Into the Spider-Verse", genre: "Animation", rating: 8.4, description: "A teen discovers his superhero potential in a multiverse." },
-    { title: "Frozen", genre: "Animation", rating: 7.4, description: "Two sisters navigate a magical adventure in Arendelle." }
+    { title: "The Lord of the Rings: The Return of the King", genre: "Fantasy", rating: 9, description: "The final battle for Middle-earth.", posterUrl: "https://image-url/inception.jpg" },
+    { title: "Star Wars: Episode V - The Empire Strikes Back", genre: "Sci-Fi", rating: 8.7, description: "The Rebel Alliance faces the mighty Empire.", posterUrl: "https://image-url/inception.jpg" },
+    { title: "Fight Club", genre: "Drama", rating: 8.8, description: "A man's ordinary life spirals into chaos.", posterUrl: "https://image-url/inception.jpg" },
+    { title: "The Lion King", genre: "Animation", rating: 8.5, description: "A young lion learns to embrace his destiny.", posterUrl: "https://image-url/inception.jpg" },
+    { title: "Black Panther", genre: "Action", rating: 7.3, description: "A new king must defend Wakanda from enemies.", posterUrl: "https://image-url/inception.jpg" },
+    { title: "Spider-Man: Into the Spider-Verse", genre: "Animation", rating: 8.4, description: "A teen discovers his superhero potential in a multiverse.", posterUrl: "https://image-url/inception.jpg" },
+    { title: "Frozen", genre: "Animation", rating: 7.4, description: "Two sisters navigate a magical adventure in Arendelle.", posterUrl: "https://image-url/inception.jpg" }
   ];
   
   return (
